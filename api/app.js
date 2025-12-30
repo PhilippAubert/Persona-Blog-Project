fdebc86c9beefe245604cfda35a257462e430598
ujin22 <p.b.m.aubert@gmail.com>
2025-12-30 16:44:23 +0100

    Set linting and add to gitignore

--- api/app.js
+++ api/app.js
@@ -12,21 +12,21 @@ const port = 3000;
 
 const server = http.createServer((req, res) => {
 
-  const cleanPath = parse(req.url).pathname;
-  const filePath = path.join(PUBLIC_DIR, cleanPath === "/" ? "index.html" : cleanPath);
-  const ext = path.extname(filePath);
+    const cleanPath = parse(req.url).pathname;
+    const filePath = path.join(PUBLIC_DIR, cleanPath === "/" ? "index.html" : cleanPath);
+    const ext = path.extname(filePath);
 
-  const contentType = ext === ".css" ? "text/css" : "text/html";
+    const contentType = ext === ".css" ? "text/css" : "text/html";
 
-  fs.readFile(filePath, "utf-8", (err, data) => {
-    if (err) {
-      res.writeHead(404, { "Content-Type": "text/plain" });
-      res.end("File not found");
-    } else {
-      res.writeHead(200, { "Content-Type": contentType });
-      res.end(data);
-    }
-  });
+    fs.readFile(filePath, "utf-8", (err, data) => {
+        if (err) {
+            res.writeHead(404, { "Content-Type": "text/plain" });
+            res.end("File not found");
+        } else {
+            res.writeHead(200, { "Content-Type": contentType });
+            res.end(data);
+        }
+    });
 });
 
 server.listen(port, () => console.log("Server running on port", port));
